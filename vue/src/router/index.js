70cdedeb704b5a91e79758d91725756d1ac14b36
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-25 01:17:18 +0700

    Validation for accessing dashboard was created

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,13 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router"
+import DefaultLayout from "../components/DefaultLayout.vue"
 import Login from "../views/Login.vue"
 import Dashboard from "../views/Dashboard.vue"
 import Registration from "../views/Registration.vue"
+import Survey from "../views/Survey.vue"
+import store from "../store"
 
 const routes = [
     {
         path: "/",
-        name: "Dashboard",
-        component: Dashboard
+        redirect: "/dashboard",
+        component: DefaultLayout,
+        meta: { requiresAuth: true }, // THIS WILL MAKE RESTRICTION IN ACCESSING DASHBOARD, IF USER HASN'T LOGGED IN, THEN HE/SHE CAN'T ACCESS THE PAGE
+        children: [
+            {
+                path: "/dashboard",
+                name: "Dashboard",
+                component: Dashboard
+            },
+            
+            {
+                path: "/survey",
+                name: "Survey",
+                component: Survey
+            }
+        ]
     },
     
     {
@@ -26,6 +43,19 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    // BELOW IS A SIMPLE VALIDATION
+    if (to.meta.requiresAuth && !store.state.user.token) 
+    {
+        next({ name: "Login" })
+    }
+
+    else 
+    {
+        next()
+    }
 })
 
 export default router
