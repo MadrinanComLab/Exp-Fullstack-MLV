6b3f3624dd3e991eed369bde94a66cd3725d03f1
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-25 18:57:48 +0700

    Reusable component was created for Dashboard and Survey component

--- vue/src/views/Survey.vue
+++ vue/src/views/Survey.vue
@@ -1,32 +1,10 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-<div class="min-h-full">
-    <header class="bg-white shadow">
-        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
-        </div>
-    </header>
-    
-    <main>
-        <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <!-- Replace with your content -->
-            <div class="px-4 py-6 sm:px-0">
-                Content goes here...
-            </div>
-            <!-- /End replace -->
-        </div>
-    </main>
-</div>
+    <PageComponent title="Survey">
+        <!--/ WHATEVER INSIDE HERE WILL BE DISPLAYED IN PAGE COMPONENT'S <SLOT> /-->
+        Content goes here...
+    </PageComponent>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+    import PageComponent from "../components/PageComponent.vue"
+</script>
