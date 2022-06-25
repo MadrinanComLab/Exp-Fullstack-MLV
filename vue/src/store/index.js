ebaaacb35eb5594ada8ae67ce7ac70d9be4f3658
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2022-06-25 19:49:45 +0700

    C:/Program Files/Git/register was created in routes/api.php

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,12 +3,8 @@ import { createStore } from "vuex"
 const store = createStore({
     state: {
         user: {
-            data: {
-                name: "Juan Dela Cruz",
-                email: "juandelacruz@example.com",
-                imgUrl: "https://thumbs.dreamstime.com/z/illustration-cute-cartoon-male-character-holding-flag-philippines-against-virus-cute-boy-cartoon-character-208966316.jpg"
-            },
-            token: 123
+            data: {},
+            token: null
         }
     },
     getters: {},
